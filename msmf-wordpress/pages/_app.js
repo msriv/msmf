b5a0060e1019919ee0ec48452237183d8db7ea99
Mihir Srivastava <msrivastava574@gmail.com>
2021-07-17 02:46:19 +0530

    lint setup and prettier hook setup

--- msmf-wordpress/pages/_app.js
+++ msmf-wordpress/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/index.css'
-
+//TODO Add navigation animation https://dev.to/anxinyang/page-transition-effect-in-nextjs-9ch
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
