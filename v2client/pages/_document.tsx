a0f619793e735ad3b83c4c77cbeaeac86be1abf8
Mihir Srivastava <msrivastava574@gmail.com>
2022-03-12 16:20:05 +0530

    font fix

--- v2client/pages/_document.tsx
+++ v2client/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link
-            href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp&display=optional"
+            href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
             rel="stylesheet"
           ></link>
           <meta name="theme-color" content={"#0E4981"} />
