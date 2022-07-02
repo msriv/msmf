816648a7042003d93a198fd3cc1ca4ed4bd2bea4
unknown <shivam.singh9804@gmail.com>
2022-07-02 13:56:12 +0530

    [server] Created Testimonial model and updated Incubatee Models by adding the testimonial getter

--- v2server/Src/Models/TestimonialsModel.php
+++ v2server/Src/Models/TestimonialsModel.php
@@ -1,7 +1,47 @@
 <?php
 
 namespace Server\Src\Models;
+use Doctorine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Testimonials")
+ */
 class TestimonialsModel {
     
+    /**
+     * @ORM\Column(type="string", options={"default" : "null"})
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $company;
+
+    /**
+     * @ORM\Column(type="string", length="400", options={"default" : "null"})
+     */
+    private $avatar;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $testimonialContent;
+
+    public funcion __construct($name, $company, $avatar, $testimonialContent){
+        $this->name = $name;
+        $this->company = $company;
+        $this->avatar = $avatar;
+        $this->testimonialContent = $testimonialContent;
+    }
+
+    public function getTestimonial (): array{
+        return array(
+            "name" => $this->name,
+            "company" => $this->company,
+            "avatar" => $this->avatar,
+            "testimonialContent" => $this->testimonialContent
+        )
+    }
 }
