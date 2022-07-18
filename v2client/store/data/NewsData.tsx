a5219b63dc668b2c908ae0776740b39642753c95
Aditya Oswal <aditya.oswal@avantika.edu.in>
2022-07-18 13:14:19 +0530

    [client] Research Events/ News Images updated

--- v2client/store/data/NewsData.tsx
+++ v2client/store/data/NewsData.tsx
@@ -2,25 +2,25 @@ import { INews } from "../../utils/Interfaces";
 
 const NewsData: Array<INews> = [
   {
-    image: "https://cdn.ms-mf.org/images/",
+    image: "https://cdn.ms-mf.org/images/Research/News/Grants_Patents.jpg",
     title: "Grants/Patents",
     about:
       "Investigator Initiated Grants on early detection, diagnostic research and AI based imaging studies (6Cr). Patents granted on molecular cytology-based detection system and nanoparticle-based delivery system",
   },
   {
-    image: "https://cdn.ms-mf.org/images/",
+    image: "https://cdn.ms-mf.org/images/Research/News/Imaging in early detection of oral cancer.jpg",
     title: "Imaging based early detection systems for oral cancer",
     about:
       "Multiple imaging systems, a portable OCT device, and a mobile-phone based system were evaluated for their efficacy in efficacy in delineation of oral potentially malignant disorders (OPMDs). The portable OCT system integrated with ANN-based analysis delineated high-grade dysplasia with high sensitivity (83%), which in turn can be employed to triage patients for tertiary care referral (James et al., 2021). Additionally, a customized dual-modality mobile oral screening device in combination with an efficient MobileNet in a lite format, achieved 81% accuracy for distinguishing normal/benign lesions from clinically suspicious lesions, using a gold standard of clinical impression based on the review of images by oral specialists.",
   },
   {
-    image: "https://cdn.ms-mf.org/images/",
+    image: "https://cdn.ms-mf.org/images/Research/News/3D system in glioblastoma.jpg",
     title: "3D cell system based on gelatin methacrylate (GelMA) hydrogels",
     about:
       "We have developed a 3D cell system based on gelatin methacrylate (GelMA) hydrogels and are studying them to understand its suitability as a 3D model system to mimic treatment-resistant mesenchymal (MES) type of GBM cells that reside in specific niches of the GBM microenvironment. Cells cultured in 3D-GMH showed higher infiltrative and invasive potential and increased chemo resistance to temozolomide (TMZ) compared to cells cultured on 2 D platform. Further, the 3D-GMH system enriched treatment-resistant mesenchymal cells that resembled the cells found in the perivascular and perinecrotic zones of glioblastoma tumors (Shah et al., 2021)",
   },
   {
-    image: "https://cdn.ms-mf.org/images/",
+    image: "https://cdn.ms-mf.org/images/Research/News/Insulin resistance model.gif",
     title:
       "Patient-derived autologous epithelial and fibroblast in vitro-model",
     about:
