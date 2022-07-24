65e4a62011a5f8d588772924172c0c795e2220d2
Aditya Oswal <aditya.oswal@avantika.edu.in>
2022-07-24 15:22:29 +0530

    [client]Implwe

--- v2client/components/Client/Philanthropy/ImpactCard.tsx
+++ v2client/components/Client/Philanthropy/ImpactCard.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Snackbar } from "@mui/material";
 import { ReactNode, useState } from "react";
-
+import ShareButtons from "../../Common/ShareButtons";
 interface ImpactCardProps {
   name: string;
   thumbnail: string;
@@ -30,49 +30,55 @@ const ImpactCard = (props: ImpactCardProps) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Link
-      href={`/philanthropy/impact#${name.split(" ").join("-").toLowerCase()}`}
-    >
+    <>
       <div className="m-4 px-4 pb-4 bg-white shadow rounded w-4/12">
-        <img
-          src={thumbnail}
-          alt={title}
-          className="m-0 w-full h-48 object-contain"
-        />
-        <p className="font-bold text-xl">{title}</p>
-        <p className="text-[#535050] text-xl">{about}</p>
-        {/* <p className="text-[#535050]">
-        <span id="amountRaised" className="font-bold text-xl">
-          {amountRaised}
-        </span>{" "}
-        raised out of{" "}
-        <span className="font-bold text-xl" id="totalAmount">
-          {totalAmount}
-        </span>
-      </p> */}
-        {/* <div className="w-full bg-gray-200 h-2 rounded">
-        <div
-          className="bg-msmf-base h-2 rounded"
-          style={{ width: "45%" }}
-        ></div>
-      </div> */}
-        {/* <div className="flex justify-between">
-        <p>
-          <span className="font-semibold">{25}</span> Days Left
-        </p>
-        <p>
-          <span className="font-semibold">{supporterCount}</span> Supporters
-        </p>
-      </div> */}
+        <Link
+          href={`/philanthropy/impact#${name
+            .split(" ")
+            .join("-")
+            .toLowerCase()}`}
+        >
+          <div>
+            <img
+              src={thumbnail}
+              alt={title}
+              className="m-0 w-full h-48 object-contain"
+            />
+            <p className="font-bold text-xl">{title}</p>
+            <p className="text-[#535050] text-xl">{about}</p>
+            {/* <p className="text-[#535050]">
+              <span id="amountRaised" className="font-bold text-xl">
+                {amountRaised}
+              </span>{" "}
+              raised out of{" "}
+              <span className="font-bold text-xl" id="totalAmount">
+                {totalAmount}
+              </span>
+            </p> */}
+            {/* <div className="w-full bg-gray-200 h-2 rounded">
+              <div
+                className="bg-msmf-base h-2 rounded"
+                style={{ width: "45%" }}
+              ></div>
+            </div> */}
+            {/* <div className="flex justify-between">
+              <p>
+                <span className="font-semibold">{25}</span> Days Left
+              </p>
+              <p>
+                <span className="font-semibold">{supporterCount}</span> Supporters
+              </p>
+            </div> */}
+          </div>
+        </Link>
         <div className="flex justify-between">
-          <button
-            onClick={() => {
-              setOpen(true);
-            }}
-            className="msmf__secondary-btn"
-          >
-            Share
-          </button>
+          <ShareButtons
+            url={`https://ms-mf.org/philanthropy/impact#${name
+              .split(" ")
+              .join("-")
+              .toLowerCase()}`}
+              title= {title}
+          />
           <button
             onClick={() => {
               window.open("/philanthropy/donate", "_self");
@@ -89,7 +95,7 @@ const ImpactCard = (props: ImpactCardProps) => {
           message="Under Development"
         />
       </div>
-    </Link>
+    </>
   );
 };
 
